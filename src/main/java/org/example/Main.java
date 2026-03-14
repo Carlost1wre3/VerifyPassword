9026dda10b872e7e53410068328fb1103497786e
Carlost1wre3 <165943344+Carlost1wre3@users.noreply.github.com>
2026-03-14 00:29:09 -0300

    Inicialização do projeto VerifyPassword

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Seja bem vindo " + nome);
+    }
+}
